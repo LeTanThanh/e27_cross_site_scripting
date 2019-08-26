ebc68f055bd2c07bdb8e98bc06c8f55923e3cafe
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-26 10:19:29 +0700

    add_posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
+  before_action :load_post, only: :show
+
   def index
-    @posts = Post.all.includes(:comments)
+    @posts = Post.includes(:comments)
+  end
+
+  def show
+    @comments = @post.comments
+  end
+
+  private
+
+  def load_post
+    @post = Post.includes(:comments).find_by(id: params[:id])
+    redirect_to root_path unless @post
   end
 end
